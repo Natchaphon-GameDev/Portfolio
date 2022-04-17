7042ce1c779a0f004a57961db1e1855f1007c5cb
Natchaphon-GameDev <nsp.gamedev@gmail.com>
2022-04-18 02:44:06 +0700

    Updates

---
+++ precache-manifest.4c2d5b47437c4525c955fb1adbb043b2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffe6ec1fa48cdd88cb099fb71bb7305a",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "b0bf08e04cd6e9f5d42a",
+    "url": "/home/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "8f25ec6f268bb3425898",
+    "url": "/home/static/js/2.e19bb7c8.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.e19bb7c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0bf08e04cd6e9f5d42a",
+    "url": "/home/static/js/main.e5447f41.chunk.js"
+  },
+  {
+    "revision": "d0b16bbb0e863459803d",
+    "url": "/home/static/js/runtime-main.a07db690.js"
+  },
+  {
+    "revision": "3c275d380d3db98e308dc6e9409c824f",
+    "url": "/home/static/media/RTM.3c275d38.png"
+  },
+  {
+    "revision": "27bc9095117f8508886ed47524b57731",
+    "url": "/home/static/media/hashirshoaeb.27bc9095.png"
+  },
+  {
+    "revision": "7eff64ce10804afa79d68598184ca649",
+    "url": "/home/static/media/resume.7eff64ce.pdf"
+  },
+  {
+    "revision": "d6b0f913ddbd52042896e0cbfe93cf7f",
+    "url": "/home/static/media/rsz_preuk.d6b0f913.png"
+  }
+]);
